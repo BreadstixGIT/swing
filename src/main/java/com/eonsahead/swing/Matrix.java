6760f0b1819cd80611e959b383072386f1c88cbc
Jack Castiglione <jhc.castiglione@gmail.com>
2020-04-06 11:04:38 -0500

    bruh moment

--- src/main/java/com/eonsahead/swing/Matrix.java
+++ src/main/java/com/eonsahead/swing/Matrix.java
@@ -7,6 +7,25 @@ package com.eonsahead.swing;
  * @version 1 April 2020
  */
 public class Matrix {
+	
+	 public static void main(String[] args) {
+	        Matrix identity = new Matrix();
+	        System.out.println("identity = " + identity);
+
+	        Matrix product = identity.multiply(identity);
+	        System.out.println("product = " + product);
+
+	        Matrix ccw = new Matrix();
+	        ccw.rotationZ(Math.PI / 4);
+	        System.out.println("counter-clockwise rotation = " + ccw);
+
+	        Matrix cw = new Matrix();
+	        cw.rotationZ(-Math.PI / 4);
+	        System.out.println("clockwise rotation = " + cw);
+
+	        Matrix netRotation = ccw.multiply(cw);
+	        System.out.println("net rotation = " + netRotation);
+	    } // main( String [] )
 
     private final double[][] elements;
 
@@ -35,6 +54,22 @@ public class Matrix {
             } // for
         } // for
     } // identity()
+    
+    public void rotationX(double angle) {
+        this.identity();
+        this.set(1, 1, Math.cos(angle));
+        this.set(1, 2, -Math.sin(angle));
+        this.set(2, 1, Math.sin(angle));
+        this.set(2, 2, Math.cos(angle));
+    } // r
+    
+    public void rotationY(double angle) {
+        this.identity();
+        this.set(0, 0, Math.cos(angle));
+        this.set(0, 2, -Math.sin(angle));
+        this.set(2, 0, Math.sin(angle));
+        this.set(2, 2, Math.cos(angle));
+    } // r
 
     public void rotationZ(double angle) {
         this.identity();
@@ -58,6 +93,18 @@ public class Matrix {
         } // for
         return product;
     } // multiply( Matrix )
+    
+    public Vector multiply(Vector vect) {
+    	Vector result = new Vector();
+    	for (int n = 0; n < 4; n++) {
+    		double set = 0;
+    		for (int m = 0; m < 4; m++) {
+    			set += vect.get(n)*this.get(n, m);
+    		}
+    		result.set(n, set);
+    	}
+    	return result;
+    }
 
     private String rowToString(int row) {
         StringBuilder result = new StringBuilder();
@@ -81,24 +128,5 @@ public class Matrix {
         result.append(" ]");
         return result.toString();
     } // toString()
-
-    public static void main(String[] args) {
-        Matrix identity = new Matrix();
-        System.out.println("identity = " + identity);
-
-        Matrix product = identity.multiply(identity);
-        System.out.println("product = " + product);
-
-        Matrix ccw = new Matrix();
-        ccw.rotationZ(Math.PI / 4);
-        System.out.println("counter-clockwise rotation = " + ccw);
-
-        Matrix cw = new Matrix();
-        cw.rotationZ(-Math.PI / 4);
-        System.out.println("clockwise rotation = " + cw);
-
-        Matrix netRotation = ccw.multiply(cw);
-        System.out.println("net rotation = " + netRotation);
-    } // main( String [] )
 
 } // Matrix
